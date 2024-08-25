7d2b4cea5caf89b56f77cbb5f8b9871c7c18dc64
Sachin <pathaksachin876@gmail.com>
2024-08-25 13:45:55 +0530

    Updates

---
+++ index.6bd02f5a.js
@@ -0,0 +1,7 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("h1", {}, "Hello World"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
